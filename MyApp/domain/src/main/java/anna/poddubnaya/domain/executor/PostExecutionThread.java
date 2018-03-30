9f679650d9b037ac259e64549c8dcb20ebc4cd4d
AnnaOlehovna <anya.olehovna@gmail.com>
2018-03-30 12:31:41 +0300

    ok

--- MyApp/domain/src/main/java/anna/poddubnaya/domain/executor/PostExecutionThread.java
+++
@@ -1,9 +0,0 @@
-package anna.poddubnaya.domain.executor;
-
-
-import io.reactivex.Scheduler;
-
-public interface PostExecutionThread {
-
-    Scheduler getScheduler();
-}
--- MyApp/domain/src/main/java/anna/poddubnaya/domain/executor/ThreadExecution.java
+++
@@ -1,7 +0,0 @@
-package anna.poddubnaya.domain.executor;
-
-import java.util.concurrent.Executor;
-
-public interface ThreadExecution extends Executor {
-
-}
