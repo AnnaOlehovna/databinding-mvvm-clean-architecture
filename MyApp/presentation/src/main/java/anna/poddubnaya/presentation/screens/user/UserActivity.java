b303ca8c42c7f36228881b378a83cb432857c98f
Anna Poddubnaya <anya.olehovna@gmail.com>
2018-04-23 21:27:10 +0300

    added location

--- MyApp/presentation/src/main/java/anna/poddubnaya/presentation/screens/user/UserActivity.java
+++ MyApp/presentation/src/main/java/anna/poddubnaya/presentation/screens/user/UserActivity.java
@@ -1,14 +1,25 @@
 package anna.poddubnaya.presentation.screens.user;
 
+import android.Manifest;
+import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.arch.lifecycle.ViewModelProviders;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.tbruyelle.rxpermissions2.RxPermissions;
 
 import java.io.File;
 
@@ -19,7 +30,12 @@ import anna.poddubnaya.presentation.databinding.ActivityUserBinding;
 import anna.poddubnaya.presentation.screens.userList.UserListRouter;
 import anna.poddubnaya.presentation.utils.ImageChooser;
 
+import static anna.poddubnaya.presentation.utils.ImageChooser.GALERIA_REQUEST_CODE;
+
 public class UserActivity extends BaseMvvmActivity<ActivityUserBinding, UserViewModel, UserRouter> {
+
+
+    private FusedLocationProviderClient fusedLocationProviderClient;
 
     @Override
     public int provideLayoutId() {
@@ -51,25 +67,27 @@ public class UserActivity extends BaseMvvmActivity<ActivityUserBinding, UserView
                 ImageChooser.startCamera(UserActivity.this);
             }
         });
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
+
     }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        if(resultCode== Activity.RESULT_OK) {
+        if (resultCode == Activity.RESULT_OK) {
             File file = ImageChooser.getImageFromResult(this, requestCode, resultCode, data);
         }
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-       getMenuInflater().inflate(R.menu.user_menu,menu);
+        getMenuInflater().inflate(R.menu.user_menu, menu);
         return super.onCreateOptionsMenu(menu);
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()){
+        switch (item.getItemId()) {
             case R.id.actionSearch:
                 break;
 
@@ -77,9 +95,33 @@ public class UserActivity extends BaseMvvmActivity<ActivityUserBinding, UserView
         return super.onOptionsItemSelected(item);
     }
 
+
     @Override
     protected void onResume() {
         super.onResume();
         viewModel.onResume();
+        RxPermissions rxPermissions = new RxPermissions(this);
+        rxPermissions
+                .request(Manifest.permission.ACCESS_FINE_LOCATION)
+                .subscribe(granted -> {
+                    if (granted) {
+                        getLastLocation();
+                    } else {
+                        // TODO
+                        // Oups permission denied
+                    }
+                });
+    }
+
+    @SuppressLint("MissingPermission")
+    private void getLastLocation() {
+        fusedLocationProviderClient.getLastLocation().addOnSuccessListener(this, new OnSuccessListener<Location>() {
+            @Override
+            public void onSuccess(Location location) {
+                if (location != null) {
+                    double width = location.getLatitude();
+                }
+            }
+        });
     }
 }
